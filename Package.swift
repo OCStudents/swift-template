2757ad0491820c32dbd0ecd82db4c84e743ed618
Mātua Doc <88599935+MatuaDoc@users.noreply.github.com>
2022-07-10 11:44:20 +1200

    Add SwiftOnslow to SwiftProgram's dependencies

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "SwiftProgram",
-            dependencies: []),
+            dependencies: ["SwiftOnslow"]),
         .testTarget(
             name: "SwiftProgramTests",
             dependencies: ["SwiftProgram"]),
